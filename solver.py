11c05f8c0a730111b5b3d778373c77c3041dc38a
Emilio Wuerges <wuerges@gmail.com>
2017-05-18 15:46:20 -0300

    should work!

--- solver.py
+++ solver.py
@@ -2,17 +2,29 @@ from model import Professor, Course, Slot, Semester, Day, Shift
 
 import pulp
 
-cs = [ Course("A"), Course("B"), Course("C"), Course("D"), Course("E"), Course("F"), Course("G"), Course("H") ]
-ps = [ Professor(cs[0:2], "P1"), Professor(cs[2:5], "P2"), Professor(cs[5:], "P3") ]
+cs = [ Course("A", 5), Course("B", 5), Course("C"), Course("D") \
+     , Course("E", 5), Course("F", 5), Course("G"), Course("H") ] 
 
+ps = [ Professor(cs[0:2], "P1"), Professor(cs[2:4], "P2") \
+      , Professor(cs[4:6], "P3"), Professor(cs[6:], "P2") ]
 
 slots = [ Slot("M1_seg", 3), Slot("M2_seg", 2), Slot("M1_ter", 3), Slot("M2_ter", 2) \
         , Slot("N1_seg", 2), Slot("N2_seg", 2), Slot("N1_ter", 2), Slot("N2_ter", 2) ]
 
-matutino =  Semester(slots[0:4], "Mat")
-noturno  =  Semester(slots[4:8], "Not")
+matutino1 =  Semester(slots[0:4], "Mat1")
+matutino1.add_course(cs[0])
+matutino1.add_course(cs[1])
+noturno2  =  Semester(slots[4:8], "Not2")
+noturno2.add_course(cs[2])
+noturno2.add_course(cs[3])
+matutino3 =  Semester(slots[0:4], "Mat3")
+matutino3.add_course(cs[4])
+matutino3.add_course(cs[5])
+noturno4  =  Semester(slots[4:8], "Mat4")
+noturno4.add_course(cs[6])
+noturno4.add_course(cs[7])
 
-semesters = [ matutino, noturno ]
+semesters = [ matutino1, noturno2, matutino3, noturno4 ]
 
 
 days = [ Day(slots[0:2] + slots[4:6], "Seg.") \
@@ -23,31 +35,63 @@ shifts = [ Shift(slots[0:2], "Mat") \
          , Shift(slots[4:6], "Not") \
          , Shift(slots[6:8], "Not") ]
 
-prof_slot = {}
+lp_vars = {}
 
 for c in cs:
     for p in ps:
         for s in slots:
-            prof_slot[(p, c, s)] = \
-                    pulp.LpVariable(p.name + "_" + c.name + "_ " + s.name, 0, 1, pulp.LpInteger)
+            for sem in semesters:
+                k = (p, c, s, sem)
+                lp_vars[k] = \
+                        pulp.LpVariable(str(k), lowBound=0, cat=pulp.LpInteger)
 
 
 def solve(professors, courses, semesters, days, shifts, slots):
-
     prob = pulp.LpProblem("Semester Problem", pulp.LpMaximize)
-    prob += pulp.lpSum([v for (k,v) in prof_slot.items()])
 
-    #print(prof_slot.keys())
+    prob += pulp.lpSum(v for k,v in lp_vars.items())
 
-    #for s in slots:
-    #    prob += pulp.lpSum([prof_slot[(p, c, s)] for p in professors for c in courses]) <= 1
+    #print(lp_vars.keys())
 
-    
-    #for p in professors:
-    #    for c in p.courses:
-    #        v = pulp.lpSum([s.size * prof_slot[(p, c, s)] for s in slots])
-    #        prob += v <= (c.num_hours + 0.1)
-    #        prob += v >= (c.num_hours - 0.1)
+    # Maximum one class for slot
+    for s in slots:
+        for sem in semesters:
+            v = pulp.lpSum(lp_vars[(p, c, s, sem)] \
+                    for p in professors \
+                    for c in courses)
+            prob += v <= 1 #s.size
+
+    # Each professor must only give his classes
+    for p in professors:
+        v = pulp.lpSum(lp_vars[(p, c, s, sem)] * s.size \
+                for s in slots \
+                for c in p.courses \
+                for sem in semesters )
+        prob += v == sum(c.num_hours for c in p.courses)
+
+    # Each proessor must not give other professor's classes
+    for p in professors:
+        v = pulp.lpSum(lp_vars[(p, c, s, sem)] * s.size \
+                for s in slots \
+                for c in courses if not c in p.courses \
+                for sem in semesters )
+        prob += v == 0
+
+    # Each course must be complete
+    for c in cs:
+        v = pulp.lpSum(lp_vars[(p, c, s, sem)] * s.size \
+                for s in slots \
+                for p in ps \
+                for sem in semesters )
+        prob += v == c.num_hours
+
+    for sem in semesters:
+        v = pulp.lpSum(lp_vars[(p, c, s, sem)] * s.size \
+                for s in slots \
+                for p in ps \
+                for c in sem.courses)
+        prob += v == sum(c.num_hours for c in sem.courses)
+
 
 
     #for p in professors:
@@ -68,8 +112,9 @@ def solve(professors, courses, semesters, days, shifts, slots):
     prob.writeLP("problem.lp")
     prob.solve()
     print("Status:", pulp.LpStatus[prob.status])
-    for v in prob.variables():
-        print(v.name, "=", v.varValue)
+    for v in prob.variables(): 
+        if v.varValue and v.varValue > 0:
+            print(v.name, "=", v.varValue)
 
 
 
